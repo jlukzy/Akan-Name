cefbe045bb0f1a8a62dc272d086ae93ddf67ddbe
Jackson Lucas <jackychaz84@gmail.com>
2020-07-18 20:02:33 +0300

    link page2.html to js script

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var DD = parseInt(document.getElementById(DD)).value
+if (DD<=0 || DD >31){
+    alert("Invalid Day,Please Enter A Valid Day")
+    }else {
+    }
+var MM = parseInt(document.getElementById(MM)).value
+if (MM<=0 || MM >12){
+    alert("Invalid Month,Please Enter A Valid Month")
+    }
+var year = parseInt(document.getElementById(year)).value
+if (year>1000){
+}else {
+alert("PLease Enter A Valid Year")
+}
