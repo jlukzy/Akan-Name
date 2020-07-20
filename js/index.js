3c0a9c1b5264b512ecbb42aacd1b440d2995c1a8
Jackson Lucas <jackychaz84@gmail.com>
2020-07-20 09:29:52 +0300

    add modified js file and remove form

--- js/index.js
+++ js/index.js
@@ -1,93 +1,80 @@
-var form = document.getElementById("birthday-form")
-form.onsubmit =function(e){
-    e.preventDefault()
-    
-    
-}
-var DD = parseInt(document.getElementById(DD)).value
+/* var DD= parseInt(prompt("Enter day"))
 if (DD<=0 || DD >31){
     alert("Invalid Day,Please Enter A Valid Day")
-    }else {
-    }
-var MM = parseInt(document.getElementById(MM)).value
+}else {
+}
+var MM=parseInt(prompt("Enter month"))
 if (MM<=0 || MM >12){
     alert("Invalid Month,Please Enter A Valid Month")
+}
+var year=parseInt(prompt("Enter year"))
+if (year<1000){
+alert("Invalid Year,PLease Enter A Valid Year")
+}
+var gender=prompt("Enter gender") */
+
+var Dayofweek=function(CC, YY, MM, DD,year){
+    return ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
     }
-var year = parseInt(document.getElementById(year)).value
-
-var gender= document.getElementById(gender).value
-function calculateDayValue(){
-    year = document.getElementById("year").value;
-    CC = parseInt(year.substring(0,2));
-    YY = parseInt(year.substring(2,4));
-    MM = parseInt(document.getElementById("MM").value);
-    DD = parseInt(document.getElementById("DD").value);
-    d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-    console.log(d);
-    return (Math.floor(d));
-    alert(d)
-  }
-var D=Math.round(d)
-if(D=0){
-    var DayOfWeek="Sunday"
-}else if(D=1){
-    var DayOfWeek="Monday"
-}else if(D=2){
-    var DayOfWeek="Tuesday"
-}else if(D=3){
-    var DayOfWeek="Wednesday"
-}else if(D=4){
-    var DayOfWeek="Thursday"
-}else if(D=5){
-    var DayOfWeek="Friday"
-}else{
-    var DayOfWeek="Saturday"
-}
-alert(DayOfWeek)
-
-if (Dayofweek===Sunday && gender===male){
-var Day=Sunday 
-var AkanName=Kwasi
-}else if(Dayofweek===Sunday && gender===female){
-var Day=Sunday
-var AkanName=Akosua
-}else if (Dayofweek===Monday && gender===male){
-var Day=Monday 
-var AkanName=Kwadwo
-}else if(Dayofweek===Monday && gender===female){
-var Day=Monday
-var AkanName=Adwoa
-}else if (Dayofweek===Tuesday && gender===male){
-var Day=Tuesday 
-var AkanName=KWabena
-}else if(Dayofweek===Tuesday && gender===female){
-var Day=Tuesday
-var AkanName=Abenaa
-}else if (Dayofweek===Wednesday && gender===male){
-var Day=Wednesday 
-var AkanName=Kwaku
-}else if(Dayofweek===Wednesday && gender===female){
-var Day=Wednesday
-var AkanName=Akua
-}else if (Dayofweek===Thursday && gender===male){
-var Day=Thursday
-var AkanName=Yaw
-}else if(Dayofweek===Thursday && gender===female){
-var Day=Thursday
-var AkanName=Yaa
-}else if (Dayofweek===Friday && gender===male){
-var Day=Friday
-var AkanName=Kofi
-}else if(Dayofweek===Friday && gender===female){
-var Day=Friday
-var AkanName=Afua
-}else if (Dayofweek===Saturday && gender===male){
-var Day=Saturday 
-var AkanName=Kwame
-}else{
-var Day=Saturday
-var AkanName=Ama
-}
-alert(Day)
-alert(AkanName)
-
+    var input = parseInt(prompt("Enter The Year You Were Born"))
+    var data = new Date(input);
+    var YY = data.getFullYear();
+    var year=[""]
+    var CC = (YY/100)+1;
+    console.log(CC);
+    var MM = parseInt(prompt("Enter The Month You Were Born"))
+    var DD = parseInt(prompt("Enter The Day You Were Born "))
+    var gender = prompt("Enter your Gender")
+    var d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7
+    console.log(d)
+    var Dayofweek= Math.round(d)
+    console.log(Dayofweek)
+    var Day=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+    var AkanName=["Kwasi", "Akosua", "Kwandwo", "Adwoa", "Kwabena", "Abenaa", "Kwaku", "AKua", "Yaw", "Yaa", "Kofi", "Afua", "Kwame", "Ama" ]
+    if (Dayofweek===0 && gender==="male"){
+    var Day=Sunday 
+    var AkanName=Kwasi
+    }else if(Dayofweek===0 && gender==="female"){
+    var Day="Sunday"
+    var AkanName="Akosua"
+    }else if (Dayofweek===1 && gender==="male"){
+    var Day="Monday" 
+    var AkanName="Kwadwo"
+    }else if(Dayofweek===1 && gender==="female"){
+    var Day="Monday"
+    var AkanName="Adwoa"
+    }else if (Dayofweek===2 && gender==="male"){
+    var Day="Tuesday" 
+    var AkanName="KWabena"
+    }else if(Dayofweek===2 && gender==="female"){
+    var Day="Tuesday"
+    var AkanName="Abenaa"
+    }else if (Dayofweek===3 && gender==="male"){
+    var Day="Wednesday" 
+    var AkanName="Kwaku"
+    }else if(Dayofweek===3 && gender==="female"){
+    var Day="Wednesday"
+    var AkanName="Akua"
+    }else if (Dayofweek===4 && gender==="male"){
+    var Day="Thursday"
+    var AkanName="Yaw"
+    }else if(Dayofweek===4 && gender==="female"){
+    var Day="Thursday"
+    var AkanName="Yaa"
+    }else if (Dayofweek===5 && gender==="male"){
+    var Day="Friday"
+    var AkanName="Kofi"
+    }else if(Dayofweek===5 && gender==="female"){
+    var Day="Friday"
+    var AkanName="Afua"
+    }else if (Dayofweek>=6 && gender==="male"){
+    var Day="Saturday" 
+    var AkanName="Kwame"
+    }else{
+    var Day="Saturday"
+    var AkanName="Ama"
+    }
+    alert(Day)
+    alert(AkanName)
+    
+    document.getElementById("output").innerHTML=compare(Day,AkanName);    
